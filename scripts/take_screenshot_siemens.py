3df0e441c85197377b7b65c663ff580b634f55f1
basecore <roesser.sebastian@gmail.com>
2026-08-06 11:12:45 +0200

    Update take_screenshot_siemens.py

--- scripts/take_screenshot_siemens.py
+++ scripts/take_screenshot_siemens.py
@@ -633,7 +633,7 @@ NON_VEG_KEYWORDS = {
     "thunfisch", "tunfisch", "matjes", "hering", "makrele", "sardine", "sardinen",
     "calamari", "tintenfisch", "pulpo", "oktopus", "garnele", "garnelen", "shrimp",
     "scampi", "muscheln", "miesmuscheln", "meerestiere", "meeresfruechte", "seafood",
-    "paella valenciana", "paella marinera", "krautwickel", "pute", "puten",
+    "paella valenciana", "paella marinera", "krautwickel", "pute", "puten", "schaschlik", "schaschlikpfanne",
 }
 
 VEG_LIKELY_KEYWORDS = {
