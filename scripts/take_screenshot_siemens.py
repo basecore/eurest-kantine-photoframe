0ffb130472a2d49c75f4e24242359f9d3a355fa1
basecore <roesser.sebastian@gmail.com>
2026-08-21 15:46:00 +0200

    Update take_screenshot_siemens.py forno added

--- scripts/take_screenshot_siemens.py
+++ scripts/take_screenshot_siemens.py
@@ -633,7 +633,7 @@ NON_VEG_KEYWORDS = {
     "thunfisch", "tunfisch", "matjes", "hering", "makrele", "sardine", "sardinen",
     "calamari", "tintenfisch", "pulpo", "oktopus", "garnele", "garnelen", "shrimp",
     "scampi", "muscheln", "miesmuscheln", "meerestiere", "meeresfruechte", "seafood",
-    "paella valenciana", "paella marinera", "krautwickel", "pute", "puten", "schaschlik", "schaschlikpfanne",
+    "paella valenciana", "paella marinera", "krautwickel", "pute", "puten", "schaschlik", "forno", "schaschlikpfanne",
 }
 
 VEG_LIKELY_KEYWORDS = {
